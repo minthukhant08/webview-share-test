8e06e096294a3e2df3591ecd875afb84ed810a1b
Min Thu Khant <minthu.khant@d3-sg.com>
2023-11-02 10:58:03 +0630

    add react native nvew trigger

--- src/app/native-share/page.tsx
+++ src/app/native-share/page.tsx
@@ -4,15 +4,18 @@ import React from 'react';
 
 const Page = () => {
     const handleShare = () => {
+      const data = {
+          url: 'https://frontend.partner.member.dev.d3lab.co',
+          title: 'native share',
+          text: 'descripton here..........'
+      }
         if(navigator.share){
-            navigator.share({
-                url: 'https://frontend.partner.member.dev.d3lab.co',
-                title: 'native share',
-                text: 'descripton here..........'
-            })
+            navigator.share(data)
         }else{
             //copy to clipboard
             navigator.clipboard.writeText('Copied text')
+            //@ts-ignore
+            window.ReactNativeWebView.postMessage(JSON.stringify(data));
             alert('copied')
         }
     }
